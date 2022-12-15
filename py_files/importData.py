2d134583c9509b6fb8d03265487b51a46a09b01e
Carbone <gianluca.carbone@ulb.be>
2022-12-15 12:25:27 +0100

    Added dataclass & pathlib
    Improved the function with logging error, pass non .wav files and corrupted files

--- py_files/importData.py
+++ py_files/importData.py
@@ -1,94 +1,99 @@
+import logging
 import os
 import soundfile as sf
 import numpy as np
-"""
-Message from 09/12
----------------------------------- WARNING -----------------------------------------------------------
 
-UPDATE REQUIRED :
-- It would be better to use the library FILEPATH instead of dealing with string. 
------> Filepath can managed automatically the proper format to create for having filepath in Windows !
-
-
-------------------------------------------------------------------------------------------------------
-"""
+from pathlib import Path
+from dataclasses import dataclass, field
 
 
 
-class AudioDataImporter():
+@dataclass
+class AudioDataImporter:
 
-    def __init__(self, user_path : str, data_path : str, ID : str, hyper_param : dict, status :str):
-        self.files_path = user_path + generate_filepath(data_path,ID,status,hyper_param['machine_name'])
-        self.files = None
-        self.file_count = None
-        self.raw_data = []
-        self.fs = 0
-        self.channel = hyper_param['channel']
-        self.hyper_param = hyper_param
-        self.status = status
-    # setters and getters   
-    def set_files(self):
-        self.files = os.listdir(self.files_path)
+    file_path : Path
+    hyper_param : dict 
+    status : str
+    
+    raw_data : list = field(init=False,default_factory=list)
+    channel : int = -1
+    files : list = field(init=False)
+    
+    def __post_init__(self):
+        # Retrieve files from folder, check if they exist
+        if not os.path.isdir(self.file_path):
+            logging.error(f"{self.file_path} does not exist")
+            raise FileNotFoundError
+        self.files = list(self.file_path.glob('**/*'))
+        # Check if all files in the directory are files
+        if not all([f.is_file() for f in self.files]):
+            logging.error(f"{self.file_path} contains folders or non-file elements")
+            raise ValueError
+        
+        # set the channel
+        self.channel = self.hyper_param['channel']
 
-    # update dictionnary of hyper parameters
-    def update_hyper_param(self,fs,status) -> dict:
+    def data_import(self, datapath : Path, min_samples = 1000):
+        """ Performs the data import on a given file. Select the channel"""
+        multi_channel_data, fs = sf.read(datapath)
+
+        # In case the data is corrupted/too small 
+        if len(multi_channel_data) < min_samples:
+            return fs, None
+        # In case only 1 channel available
+        if multi_channel_data.ndim <= 1:
+            return fs, multi_channel_data
+        # In case multiple channel are accessible
+        return fs, np.array(multi_channel_data)[:,self.channel]
+        
+
+    def _update_hyper_param(self,fs) -> dict:
         """ Update the hyper parameters dictionnary """
         file_count_str = 'file_count_' + self.status
         self.hyper_param[file_count_str] =  len(self.files)
         self.hyper_param['fs'] =  fs
 
-    def VerifyFileExist(self,file):
-        """ Checks that the file exists"""
-        return os.path.isfile(os.path.join(self.files_path, file))
-
-    
-    def data_import(self, data_path):
-        """ Performs the data import on a given file. Select the channel"""
-        try:
-            multi_channel_data, fs = sf.read(data_path)
-            if multi_channel_data.ndim <= 1:
-                # ADD HERE A CONDITION TO READ MULTICHANNEL LENGTH AND REMOVE IF TOO SHORT
-                return fs, multi_channel_data
-            return fs, np.array(multi_channel_data)[:,self.channel]
-        except ValueError:
-            pass
-
-    def foo(self):
+    def _set_raw_data(self):
         """ Import each file located at a specific filePath (folder), import and save them in a list.
             Updates hyper-param dict
         """
-        self.set_files()
         for file in self.files:
-            if self.VerifyFileExist(file):
+            if file.suffix != ".wav":
+                    logging.warning(f"Ignoring file {file} as it is not a .wav file")
+            else:
+                fs, raw_data_vector = self.data_import(file)
+                if raw_data_vector is not None:
+                    self.raw_data.append(raw_data_vector)
 
-                fs, raw_data_vector = self.data_import(os.path.join(self.files_path,file))
-                self.raw_data.append(raw_data_vector)
 
-        self.update_hyper_param(fs, self.status)
+        self._update_hyper_param(fs)
 
         return self.hyper_param, self.raw_data
 
-
-def generate_filepath(data_path : str, ID : str,status : str, machine_name : str) -> str:
-    """ Generate the filepath to get the audio file """
-    
-    return data_path + machine_name + '/' + ID + '/' + status
-
-    
-
+def generate_filepath(data_path : str, machine_name : str, id : str, status : str) -> str:
+        """ Generate the filepath in strings to get the audio file """
+        return Path(data_path + machine_name + '/' + id + '/' + status)
 
 def main() -> None:
 
     user_path = 'C:/Users/carbo/Documents/'
-    DATAPATH_NORMAL  = "/MIMII/RawData/+6dB/"
-    ID= 'id_02'
+    data_folder  = "/MIMII/RawData/+6dB/"
 
-    FILEPATH_ABNORMAL  = "/MIMII/RawData/+6dB/test/id_02/abnormal"
+    data_path = user_path + data_folder
+
+    id= 'id_02'
+
     hyper_param = {"channel" : 2,
                     "machine_name" : 'test'}
 
-    audio_imp = AudioDataImporter(user_path, DATAPATH_NORMAL,ID,hyper_param , "normal")
-    hyper_param, raw_data = audio_imp.foo()
+
+    file_path = generate_filepath(data_path = data_path, machine_name= hyper_param['machine_name'], id = id , status ="normal")
+
+    print(file_path)
+
+    audio_imp = AudioDataImporter(file_path = file_path, hyper_param =hyper_param, status ="normal")
+
+    hyper_param, raw_data = audio_imp._set_raw_data()
     
     print(f'The updated hyper_param dictionnary is {audio_imp.hyper_param}')
     print(f'The raw data is a array of {len(raw_data)} lists each containing {len(raw_data[0])} elements')
